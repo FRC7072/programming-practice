a62f7dc749de14a887ea937207ca705f74dae198
Logan Howell <loganshowell@gmail.com>
2018-01-13 13:36:12 -0600

    Updated for 2018 libraries

--- src/org/usfirst/frc/team7072/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team7072/robot/subsystems/DriveTrain.java
@@ -1,32 +1,32 @@
 package org.usfirst.frc.team7072.robot.subsystems;
 
-import java.rmi.server.SkeletonNotFoundException;
-
 import org.usfirst.frc.team7072.robot.RobotMap;
 import org.usfirst.frc.team7072.robot.commands.DriveWithJoystick;
 
-import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class DriveTrain extends Subsystem {
 	
-	public CANTalon leftFrontMotor = new CANTalon(RobotMap.leftFrontMotor);
-	public CANTalon leftBackMotor = new CANTalon(RobotMap.leftBackMotor);
-	public CANTalon rightFrontMotor = new CANTalon(RobotMap.rightFrontMotor);
-	public CANTalon rightBackMotor = new CANTalon(RobotMap.rightBackMotor);
+	public PWMTalonSRX leftFrontMotor = new PWMTalonSRX(RobotMap.leftFrontMotor);
+	public PWMTalonSRX leftBackMotor = new PWMTalonSRX(RobotMap.leftBackMotor);
+	public PWMTalonSRX rightFrontMotor = new PWMTalonSRX(RobotMap.rightFrontMotor);
+	public PWMTalonSRX rightBackMotor = new PWMTalonSRX(RobotMap.rightBackMotor);
+	
+	public SpeedControllerGroup leftMotors = new SpeedControllerGroup(leftFrontMotor, leftBackMotor);
+	public SpeedControllerGroup rightMotors = new SpeedControllerGroup(rightFrontMotor, rightBackMotor);
 	
 	//Need both front and back drives working together since there are two motor on
 	// each side powering the same wheels.
-	private RobotDrive frontDrive;
-	private RobotDrive backDrive;
+	private DifferentialDrive drive;
 	
 	public DriveTrain() {
-		// TODO Auto-generated constructor stub
-		frontDrive = new RobotDrive(leftFrontMotor, rightFrontMotor);
-		backDrive = new RobotDrive(leftBackMotor, rightBackMotor);
+		drive = new DifferentialDrive(leftMotors, rightMotors);
 	}
 	
 	@Override
@@ -40,12 +40,10 @@ public class DriveTrain extends Subsystem {
 	}
 	
 	public void tankDrive(double leftValue, double rightValue) {
-		frontDrive.tankDrive(leftValue, rightValue, true);
-		backDrive.tankDrive(leftValue, rightValue, true);
+		drive.tankDrive(leftValue, rightValue, true);
 	}
 	
 	public void stop() {
-		frontDrive.tankDrive(0, 0);
-		backDrive.tankDrive(0, 0);
+		drive.tankDrive(0, 0);
 	}
 }
